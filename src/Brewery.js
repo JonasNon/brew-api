588d8593928b41e0e29600d90441b67ff83816bc
JonasNon <officialjonasn@gmail.com>
2024-08-10 22:46:28 -0500

    made styling better

--- src/Brewery.js
+++ src/Brewery.js
@@ -13,7 +13,8 @@ const iFrameStyles = {
 const rightStyles = {
   position: "abolute",
   left: "30vw",
-  width: "80vw"
+  width: "80vw",
+  right: 0
 }
 
 
@@ -24,11 +25,11 @@ function Brewery (props) {
   return (
     <div id="upper">
       <iframe scrolling="no" style={iFrameStyles} src={beer.website_url} title="description"></iframe>
-      <div style={rightStyles}>
+      <div id="right" style={rightStyles}>
         <h3 key={index}>{beer.name}</h3>
         <span> Brewery type: {beer.brewery_type}</span>
         <h4>Phone: {beer.phone}</h4>
-        <p>Street: {beer.street} <span> City: {beer.city}</span> Website: {beer.website_url}</p> 
+        <p>Street: {beer.street} <span> City: {beer.city}</span> Website: <a href={beer.website_url}>{beer.website_url}</a></p> 
       </div>
     </div>
 
